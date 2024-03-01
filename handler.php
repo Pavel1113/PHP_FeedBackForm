9183d4a51718c2a07a0c6127dea18ee8da292471
Bich Pavel <bichpasha@yandex.ru>
2024-03-01 10:31:24 +0300

    Added button to select all appeals

--- handler.php
+++ handler.php
@@ -1,13 +1,13 @@
 <?php
 require_once 'config.php';
-$name = $_POST['name'];
-$email = $_POST['email'];
-$tel = $_POST['tel'];
-$message = $_POST['message'];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (!empty($_POST)) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $tel = $_POST['tel'];
+        $message = $_POST['message'];
         $connection->query("INSERT INTO appeals (name, email, phone, message) VALUES ('$name', '$email', '$tel', '$message')");
         ?>
         <b style="color: green">Ваше сообщение было отправлено!</b>
@@ -17,15 +17,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <li>Ваш телефон: <?php echo $tel?></li>
             <li>Ваше сообщение: <?php echo $message?></li>
         </ul>
-        <a href="index.html">Назад</a>
+        <a href="index.php">Назад</a>
 <?php
     } else {
         echo "Данные формы не были отправлены.";
     }
 } else if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $result = $connection->query("SELECT * FROM appeals");
-    $text = $result->fetch_assoc();
-    var_dump($result);
-    var_dump($text);
+    $data = [];
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    $table = "<table><tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Message</th>
+                    </tr>";
+    foreach($data as $value) {
+        $table .= "<tr>";
+        $table .= "<td>".$value['id']."</td>";
+        $table .= "<td>".$value['name']."</td>";
+        $table .= "<td>".$value['email']."</td>";
+        $table .= "<td>".$value['phone']."</td>";
+        $table .= "<td>".$value['message']."</td>";
+        $table .= "</tr>";
+    }
+    $table .= "</table>";
+    echo $table;
 }
 ?>
